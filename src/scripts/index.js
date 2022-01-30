66963bc50dd4002ef88a8960258a358a4cf3e305
Honma Ryota <m95.honma.ryota@gmail.com>
2022-01-31 01:36:04 +0900

    Deonatulle 完成

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,24 @@
-const btn = document.querySelector("#topBtn");
-
-btn.addEventListener("click", function () {
+// Top scroll
+const topBtn = document.querySelector("#topBtn");
+topBtn.addEventListener("click", function () {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 });
+
+// Item scroll
+const itemBtn = document.querySelector(".link");
+const item = document.querySelector(".item");
+itemBtn.addEventListener("click", function () {
+  item.scrollIntoView({
+    block: "center",
+    behavior: "smooth",
+  });
+});
+
+// loader
+window.onload = function () {
+  const spinner = document.getElementById("loader-bg");
+  spinner.classList.add("loaded");
+};
